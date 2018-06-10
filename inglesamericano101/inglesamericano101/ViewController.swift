9c1ddcb0aa2ce3800587d5c4c93c404c66e39151
Antonio Cortes <antoni_anemi@hotmail.com>
2018-06-10 14:58:39 -0700

    autentication with firabase integrated.

--- inglesamericano101/inglesamericano101/ViewController.swift
+++ inglesamericano101/inglesamericano101/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
-
+import Firebase
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txt_username: UITextField!
+    @IBOutlet weak var txt_password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func logIn(){
+        guard let username = txt_username.text,let password = txt_password.text else { return }
+        Auth.auth().signIn(withEmail: username, password: password) { (user, error) in
+            if error != nil{
+                
+            }
+        }
+    }
 
 }
 
